c451f43eb8b166d8ea45a2c05717376658c0816d
Isabella <collantemendezisabella@gmail.com>
2023-06-09 15:17:46 -0500

    ingecosmo_vb

---
+++ ingecosmo/Conectar.vb
@@ -0,0 +1,16 @@
+﻿Imports MySql.Data.MySqlClient
+Module Conectar
+    Public con As String = "Server=localhost; database=db_ingecosmo; User ID=root; Password=; "
+    Public mycon As MySqlConnection = New MySqlConnection(con)
+    Public rs As MySqlCommand = New MySqlCommand
+    Public Function Prueba() As Boolean
+        Try
+            mycon.Open()
+            Return True
+        Catch ex As MySqlException
+            MessageBox.Show("No se pudo conectar al servidor")
+            Return False
+        End Try
+    End Function
+
+End Module
---
+++ ingecosmo/Form1.vb
@@ -0,0 +1,25 @@
+﻿
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim user As String
+    Dim pass As String
+
+    Private Sub Btn_entra_Click_1(sender As Object, e As EventArgs) Handles Btn_entra.Click
+        Try
+            user = txt_usuario.Text
+            pass = txt_contra.Text
+            If Conectar.Prueba() Then
+                MessageBox.Show("Usuario ")
+                Me.Hide()
+                principal.Show()
+            End If
+
+        Catch ex As MySqlException
+            MessageBox.Show("Usuario sin autorizacion en el sistema")
+
+        End Try
+
+
+    End Sub
+End Class
